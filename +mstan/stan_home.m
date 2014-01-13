c8624a9bff78db2640ef05eda99997f1a2079f1a
Brian Lau <brian.lau@upmc.fr>
2014-01-13 12:57:50 +0100

    update to CmdStan 2.1.0

--- +mstan/stan_home.m
+++ +mstan/stan_home.m
@@ -1,3 +1,5 @@
+% Modify the string below so that it points to the parent directory of 
+% your CmdStan installation
 function d = stan_home()
 
-d = '/Users/brian/Downloads/stan-2.0.1';
+d = '/Users/brian/Downloads/stan-2.1.0';
