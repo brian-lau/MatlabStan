31a1021f92e38cfdc3aa79d2bb8712e4783a0b0d
Brian Lau <brian.lau@upmc.fr>
2015-02-20 19:00:40 +0100

    Update tests for Stan 2.6.0

--- Tests/TestBernoulli.m
+++ Tests/TestBernoulli.m
@@ -50,7 +50,11 @@ classdef TestBernoulli < TestCase
          model = self.model;
          assertEqual(model.model_name,'bernoulli');
          assertEqual(model.model_code,self.code);
-         assertTrue(exist('bernoulli.cpp','file')==2,'CPP file not generated');
+         if mstan.check_ver(model.stan_version,'2.6.0')
+             assertTrue(exist('bernoulli.hpp','file')==2,'HPP file not generated');
+         else
+             assertTrue(exist('bernoulli.cpp','file')==2,'CPP file not generated');
+         end
          if ispc
             [~,fa] = fileattrib('bernoulli.exe');
          else
